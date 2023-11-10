cd3a6145803c3aa95a95902132c8fdf42547e909
CamiloDev <camiloanla@gmail.com>
2023-11-09 22:35:18 -0500

    IMPLEMENTACIÓN - vista venta
    -Se inicia el  modulo de venta

--- Ventas.php
+++ Ventas.php
@@ -3,9 +3,58 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Document</title>
 </head>
-<body>
-    <h1>BIENVENIDO ACA PODRA COMPRAR</h1>
+<body style="background-color: #8470e7;">
+    
+    <div class="container mt-5">
+    <div class="row">
+
+        <!-- Primer Producto -->
+        <div class="col-md-4 mb-4">
+            
+            <div class="card">
+                <img src="img/CarroLogin.jpg" class="card-img-top" alt="Producto 1">
+                <div class="card-body">
+                    <h5 class="card-title">Producto 1</h5>
+                    <p class="card-text">Descripción del Producto 1.</p>
+                    <a href="#" class="btn btn-primary">Comprar</a>
+                </div>
+            </div>
+        </div>
+
+        <!-- Segundo Producto -->
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img src="img/CarroLogin.jpg" class="card-img-top" alt="Producto 2">
+                <div class="card-body">
+                    <h5 class="card-title">Producto 2</h5>
+                    <p class="card-text">Descripción del Producto 2.</p>
+                    <a href="#" class="btn btn-primary">Comprar</a>
+                </div>
+            </div>
+        </div>
+
+        <!-- Tercer Producto -->
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img src="img/CarroLogin.jpg" class="card-img-top" alt="Producto 3">
+                <div class="card-body">
+                    <h5 class="card-title">Producto 3</h5>
+                    <p class="card-text">Descripción del Producto 3.</p>
+                    <a href="#" class="btn btn-primary">Comprar</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+    
 </body>
 </html>
