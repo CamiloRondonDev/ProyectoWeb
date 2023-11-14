4ee00aa085cb8677b29576206448c0707ddb14ac
Fabiana soto <karolfabianaduran@gmail.com>
2023-11-13 20:16:12 -0500

    AJUSTE - Se agregan elementos al diseño he imagenes en ventas

--- Ventas.php
+++ Ventas.php
@@ -8,11 +8,69 @@
     <title>Document</title>
 </head>
 <body style="background-color: #8470e7;">
-    
-    <div class="container mt-5">
+    <!-- inicio menu -->
+    <div class=container>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><img src="img/SuperAutos.PNG" class="img-fluid"""></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </div>
+    <!-- final menu  -->
+    <br>
+    <!-- vehiculos -->
+    <div class= "container">
+     <div class="row">
+        <div class="col-md-4 text-center text-bg-secondary p-3">
+          <img src="img/ChevroletCamaroXL1.PNG" class="img-fluid">
+          <h2>Nombre carro</h2>
+          <p>$.0000</p>
+          <a href="" class="btn btn-primary">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+              <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+            </svg>
+          </a>
+        </div>
+     </div>
+    </div>
+    <!-- fin vehiculos -->
+
+
+    <!-- <div class="container mt-5">
     <div class="row">
 
-        <!-- Primer Producto -->
+         Primer Producto
         <div class="col-md-4 mb-4">
             
             <div class="card">
@@ -25,7 +83,7 @@
             </div>
         </div>
 
-        <!-- Segundo Producto -->
+         Segundo Producto 
         <div class="col-md-4 mb-4">
             <div class="card">
                 <img src="img/CarroLogin.jpg" class="card-img-top" alt="Producto 2">
@@ -37,7 +95,7 @@
             </div>
         </div>
 
-        <!-- Tercer Producto -->
+        Tercer Producto
         <div class="col-md-4 mb-4">
             <div class="card">
                 <img src="img/CarroLogin.jpg" class="card-img-top" alt="Producto 3">
@@ -49,7 +107,7 @@
             </div>
         </div>
 
-    </div>
+    </div> !-->
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
